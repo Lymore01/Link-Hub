88b3c3dbece7b5a5625ca14d18f7112f785d241c
Lymore01 <kellytoroitich89@gmail.com>
2024-04-06 13:19:47 +0300

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <p className = "text-[red]">Hello Limo</p>
+    </>
+  );
+}
+
+export default App;
